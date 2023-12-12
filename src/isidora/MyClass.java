365d540f0b4fcda8192d267d8547b3427932ef71
MagdalenaDimcevska <147504217+MagdalenaDimcevska@users.noreply.github.com>
2023-12-12 13:31:42 +0100

    Add first java project

---
+++ src/isidora/MyClass.java
@@ -0,0 +1,8 @@
+package isidora;
+public class MyClass {
+	public static void main(String[] args) {
+		Vehicle m = new Vehicle();
+		System.out.println(m);
+		Vehicle m1= new Vehicle("blue");
+		System.out.println(m1);
+		}}
---
+++ src/isidora/Vehicle.java
@@ -0,0 +1,12 @@
+package isidora;
+public class Vehicle {
+private String color;
+	Vehicle(){
+		color="red";
+		}
+Vehicle(String r){
+		this.color = r;
+}
+public String toString() {
+    return color;
+}}
